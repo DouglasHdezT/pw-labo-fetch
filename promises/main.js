c44c1b5c7be0542cdb48d986fd1033bf394cecf1
DouglasHdezT <00004516@uca.edu.sv>
2020-10-07 23:32:13 -0600

    Add: Promises examples

--- promises/main.js
+++ promises/main.js
@@ -21,7 +21,7 @@ const promesa = () => {
 
 promesa()
     .then((value) => {
-        console.log(value);
+        console.log(`Promise: ${value}`);
     })
     .catch((error) => { 
         console.log(`Error: ${error}`);
@@ -39,13 +39,13 @@ const promesa2 = (number) => {
             } else {
                 resolve("Numero par aceptado");
             }
-        }, 200);
+        }, 1000);
     });
 }
 
 promesa2(4)
     .then((value) => { 
-        console.log(value);
+        console.log(`Promise2: ${value}`);
     })
     .catch((error) => { 
         console.error(`Error: ${error}`);
@@ -56,3 +56,39 @@ promesa2(4)
  * Primero lo primero: Async
  */
 
+const asyncFun = async (number) => { 
+    /* if (number % 2 === 0) {
+        return "Numero par asincrono aceptado";
+    } else { 
+        throw new Error("Numero impar asincrono denegado")
+    } */
+    
+    if (number % 2 !== 0) {
+        throw new Error("Numero impar asincrono denegado")
+    }
+    
+    return "Numero par asincrono aceptado";
+}
+
+asyncFun(2)
+    .then(value => console.log(`Async: ${value}`))
+    .catch(err => console.log(`Error: ${err.message}`));
+
+/**
+ * Uso de await
+ */
+const awaiting = async () => { 
+    try {
+        console.log("Awaiting: Checkpoint 1");
+
+        //const result = await promesa2(1);
+        const result = await asyncFun(2);
+        console.log(`Awaiting: ${result}`)
+
+        console.log("Awaiting: Checkpoint 2");
+    } catch (error) {
+        console.log(`Awaiting: ${error.message}`)
+    }
+}
+
+awaiting();
