93f13b1a955115f8c4990e0fab78dec140b04316
DouglasHdezT <00004516@uca.edu.sv>
2020-10-08 19:05:08 -0600

    Fix: merge problems

--- fetch-core/main.js
+++ fetch-core/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const BASE_URL = "https://pokeapi.co/api/v2";
 
 let fetchEx1 = () => { 
@@ -17,35 +16,10 @@ let fetchEx1 = () => {
             console.log(data);
         })
         .catch(err => console.error("Algo salio mal xc"));
-=======
-/**
- * fetch -> API de navegador
- */
-
-const BASE_URL = "https://pokeapi.co/api/v2";
-
-let fetch1 = () => {
-    const uri = `${BASE_URL}/pokemon/ditto`;
-
-    fetch(uri)
-        .then(response => { 
-            console.log(response);
-            /** TODO: Verificar estados, validar headers, etc */
-            
-            return response.json();
-        })
-        .then(data => { 
-            console.log(data)
-        })
-        .catch(error => { 
-            console.log("Algo ocurrio mal :/");
-        });
->>>>>>> bareProject
 }
 
 let fetch2 = async () => { 
     try {
-<<<<<<< HEAD
         const uri = `${BASE_URL}/pokemon/`;
 
         const response = await fetch(uri);
@@ -63,23 +37,3 @@ let fetch2 = async () => {
         console.error("Algo salio mal :v");
     }
 }
-=======
-        const uri = `${BASE_URL}/pokemon/ditto`;
-
-        const response = await fetch(uri); 
-        console.log(response);
-
-        //TODO: Validar todo lo que se deba validar
-
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-        } else { 
-            console.log(response.status);
-        }
-
-    } catch (error) {
-        console.log("Algo salió mal :/");
-    }
-}
->>>>>>> bareProject
--- promises/main.js
+++ promises/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const promesa = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -31,54 +30,11 @@ const promise2 = (number) => {
                 resolve("Numero par aceptado");
             } else { 
                 reject("Numero impar denegado")
-=======
-/**
- * Promesas como tal
- */
-
-const promesa = () => { 
-    return new Promise((resolve, reject) => { 
-        //Random number
-        setTimeout(() => {
-            const number = Math.floor(Math.random() * 2);
-            
-            if (number === 0) {
-                reject("No acepto ceros");
-            } else {
-                resolve("Numero aceptado");
-            }
-        }, 200);
-    })
-}
-
-/* console.log(promesa()); */
-
-promesa()
-    .then((value) => {
-        console.log(`Promise: ${value}`);
-    })
-    .catch((error) => { 
-        console.log(`Error: ${error}`);
-    });
-
-/**
- * Promesa con parámetros
- */
-
-const promesa2 = (number) => { 
-    return new Promise((resolve, reject) => { 
-        setTimeout(() => { 
-            if (number % 2 !== 0) {
-                reject("No acepto impares");
-            } else {
-                resolve("Numero par aceptado");
->>>>>>> bareProject
             }
         }, 1000);
     });
 }
 
-<<<<<<< HEAD
 promise2(1)
     .then(value => console.log(`Promise 2: ${value}`))
     .catch(error => console.log(`Promise 2 Error: ${error}`))
@@ -117,53 +73,6 @@ const awaiting = async () => {
         console.error(`Await: ${error}`)
     } finally { 
         console.log("Await: Siempre me ejecuto")
-=======
-promesa2(4)
-    .then((value) => { 
-        console.log(`Promise2: ${value}`);
-    })
-    .catch((error) => { 
-        console.error(`Error: ${error}`);
-    });
-
-/**
- * La maravillosa aparición de async y await
- * Primero lo primero: Async
- */
-
-const asyncFun = async (number) => { 
-    /* if (number % 2 === 0) {
-        return "Numero par asincrono aceptado";
-    } else { 
-        throw new Error("Numero impar asincrono denegado")
-    } */
-    
-    if (number % 2 !== 0) {
-        throw new Error("Numero impar asincrono denegado")
-    }
-    
-    return "Numero par asincrono aceptado";
-}
-
-asyncFun(2)
-    .then(value => console.log(`Async: ${value}`))
-    .catch(err => console.log(`Error: ${err.message}`));
-
-/**
- * Uso de await
- */
-const awaiting = async () => { 
-    try {
-        console.log("Awaiting: Checkpoint 1");
-
-        //const result = await promesa2(1);
-        const result = await asyncFun(2);
-        console.log(`Awaiting: ${result}`)
-
-        console.log("Awaiting: Checkpoint 2");
-    } catch (error) {
-        console.log(`Awaiting: ${error.message}`)
->>>>>>> bareProject
     }
 }
 
